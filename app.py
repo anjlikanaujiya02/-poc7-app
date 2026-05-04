fc0b9c3a291d36d06db291d8aa7ea1132b03a2ba
anjlikanaujiya02 <anjlikanaujiya02@gmail.com>
2026-05-04 15:19:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "POC-7 Jenkins Docker Ansible Pipeline Works!"
+    return "POC-7 Jenkins Docker Ansible Pipeline Works!...Done"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
